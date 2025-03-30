30eb576d971b6b8df4dbfa1adc7f2f9ac065bb4a
Sifonio <152066301+Sifonio@users.noreply.github.com>
2025-03-30 15:17:59 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
     void *ptr = NULL;
-    int i = 0;
-    while (i < 3) {
+    for (int i = 0; i < 3; i++) {
         if (!ptr)
-            ptr = malloc(10);
-        if (!ptr) {
-            printf("Помилка виділення пам'яті\n");
-            return 1;
+            ptr = malloc(10); 
+        
+        if (ptr) {
+            printf("Ітерація %d: ptr = %p\n", i, ptr);
+            ((char *)ptr)[0] = 'A';  
         }
-        printf("Ітерація %d, ptr = %p\n", i, ptr);
-        free(ptr); 
-        i++;
+
+        free(ptr);  
+        ptr = NULL;
     }
+
     return 0;
 }
 //gcc -Wall 4.c -o 4
